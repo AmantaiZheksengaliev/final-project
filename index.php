013193b291e131a60dccdc376c3966bcf60776ba
AmantaiZheksengaliev <43029686+AmantaiZheksengaliev@users.noreply.github.com>
2018-12-23 20:55:47 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="nav-bar">
+        <ul class="primary-menu-list">
+            <li class="active"><a href="index.php">Blog</a></li>
+            <li><a href="new-post.php">New post</a></li>
+        </ul>
+
+        <ul class="auth-list">
+            <li><a href="profile.php">Kaisar Kuanysh</a></li>
+        </ul>
+    </div>
+
+    <section id="content">
+        <h1>Blog:</h1>
+        <div class="post">
+            <h3>Post header</h3>
+            <hr>
+        </div>
+    </section>
+</body>
+</html>
